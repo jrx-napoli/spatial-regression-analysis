a21b80be52688ef5ce1da502f222c841f5166465
jrx-napoli <janek.rybarczyk@gmail.com>
2024-06-09 23:15:42 +0200

    code documentation

--- spatial_regression_comparison.py
+++ spatial_regression_comparison.py
@@ -9,7 +9,40 @@ from spreg import OLS, GM_Error, ML_Error, GM_Lag, ML_Lag, GM_Error_Het, GM_Erro
 
 
 class SpatialRegressionComparison:
-    def __init__(self, x, y, w, y_name, x_names, w_name, dataset, dataset_name, q=None, y_end=None, regimes=None,
+    """
+    A class to compare various spatial regression models on a given dataset.
+
+    Attributes:
+        X (numpy array): The independent variables.
+        y (numpy array): The dependent variable.
+        weights (numpy array): Spatial weight's matrix.
+        q (numpy array): Instrumental variables for certain models.
+        y_end (numpy array): Endogenous variables for regimes models.
+        regimes (list): Regimes for each observation.
+        y_name (str): Name of the dependent variable.
+        x_names (list): Names of the independent variables.
+        w_name (str): Name of the weight's matrix.
+        regimes_name (str): Name of the regimes variable.
+        dataset (DataFrame): The dataset being used.
+        dataset_name (str): Name of the dataset.
+        models (dict): Dictionary to store the fitted models.
+        mse_results (dict): Dictionary to store mean squared error results.
+        execution_times (dict): Dictionary to store execution times of models.
+        results_path (str): Path to store the results.
+    """
+
+    def __init__(self,
+                 x,
+                 y,
+                 w,
+                 y_name,
+                 x_names,
+                 w_name,
+                 dataset,
+                 dataset_name,
+                 q=None,
+                 y_end=None,
+                 regimes=None,
                  regimes_name=None):
         self.X = x
         self.y = y
@@ -30,8 +63,21 @@ class SpatialRegressionComparison:
         self.results_path = "results"
 
     def run_model(self, model_name, model_class, *args):
+        """
+        Runs a specified spatial regression model and records its execution time.
+
+        Parameters:
+            model_name (str): The name of the model to be run.
+            model_class (class): The class of the model to be run.
+            *args: Additional arguments required by the model.
+        """
         start_time = time.time()
-        model = model_class(self.y, self.X, *args, name_y=self.y_name, name_x=self.x_names, name_w=self.w_name,
+        model = model_class(self.y,
+                            self.X,
+                            *args,
+                            name_y=self.y_name,
+                            name_x=self.x_names,
+                            name_w=self.w_name,
                             name_ds=self.dataset_name)
         end_time = time.time()
 
@@ -39,6 +85,9 @@ class SpatialRegressionComparison:
         self.execution_times[model_name] = np.round(end_time - start_time, 3)
 
     def run_all_models(self):
+        """
+        Runs all spatial regression models.
+        """
         # Spatial regression models
         self.run_model('OLS', OLS)
         self.run_model('ML_Lag', ML_Lag, self.weights)
@@ -60,6 +109,9 @@ class SpatialRegressionComparison:
         self.run_model('GM_Error_Hom_Regimes', GM_Error_Hom_Regimes, self.regimes, self.weights)
 
     def compare_models(self):
+        """
+        Compares all fitted models based on their mean squared error and execution time, generates reports.
+        """
         if not os.path.exists(self.results_path):
             os.makedirs(self.results_path)
 
@@ -78,6 +130,9 @@ class SpatialRegressionComparison:
         self._plot_mse_results()
 
     def _create_reports(self):
+        """
+        Creates and saves comparison reports for mean squared error and execution time.
+        """
         f_comp_error = open(f"{self.results_path}/comparison_error_{self.dataset_name}.txt", "w")
         f_comp_error.write(F"{self.dataset_name}: MEAN SQUARED ERROR COMPARISON\n")
         f_comp_error.write(F"--------------------------------------------------\n\n")
@@ -95,6 +150,9 @@ class SpatialRegressionComparison:
             f_comp_time.write(f"{name}: {self.execution_times[name]}\n")
 
     def _plot_execution_times(self):
+        """
+        Plots a bar chart of execution times.
+        """
         plt.figure(figsize=(10, 6))
         plt.bar(self.execution_times.keys(), self.execution_times.values(), color='skyblue')
         plt.xlabel('Model')
@@ -107,6 +165,9 @@ class SpatialRegressionComparison:
         plt.close()
 
     def _plot_mse_results(self):
+        """
+        Plots a bar chart of mean squared errors.
+        """
         plt.figure(figsize=(10, 6))
         plt.bar(self.mse_results.keys(), self.mse_results.values(), color='skyblue')
         plt.xlabel('Model')

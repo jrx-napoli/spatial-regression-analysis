39347f23e107065d86a235dff288341f8f312b5a
jrx-napoli <janek.rybarczyk@gmail.com>
2024-06-11 22:02:56 +0200

    adjusted default options

--- options.py
+++ options.py
@@ -4,7 +4,7 @@ import argparse
 def get_args(argv):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--dataset', type=str, default='synthetic', help='Dataset to be used in experiment')
+    parser.add_argument('--dataset', type=str, default='baltimore', help='Dataset to be used in experiment')
     parser.add_argument('--n', type=int, default=100, help='Number of observations')
     parser.add_argument('--k', type=int, default=10, help='Number of independent variables')
     parser.add_argument('--regime_k', type=int, default=2, help='Number of independent variables')
